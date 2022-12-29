a800fa49949fce01fa8264f294e7727c315a0c65
Kyaw Ye Htet <kyawyehtet@amdon.com>
2022-12-29 12:04:20 +0630

    config register platform

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const routes = require('./src/routes')
 
 const lti = require('ltijs').Provider
 
-
 // Setup
-lti.setup(process.env.LTI_KEY,
+lti.setup(
+  process.env.LTI_KEY,
   {
-    url: 'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin', // connection to mongodb
-  }, {
-  staticPath: path.join(__dirname, './public'), // Path to static files
-  cookies: {
-    secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
-    sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+    url: process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin' // connection to mongodb
   },
-  devMode: false // Set DevMode to true if the testing platform is in a different domain and https is not being used
-})
+  {
+    staticPath: path.join(__dirname, './public'), // Path to static files
+    cookies: {
+      secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
+      sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+    },
+    devMode: false // Set DevMode to true if the testing platform is in a different domain and https is not being used
+  }
+)
 
 // When receiving successful LTI launch redirects to app
 lti.onConnect(async (token, req, res) => {
@@ -40,14 +42,16 @@ const setup = async () => {
    */
   await lti.registerPlatform({
     url: 'https://canvas.instructure.com', // or url : 'https://canvas.exampledomain.com' (depends on config form Canvas instance) if iss is changed in config/security.yml file! It must be the same as the iss
-    name: 'exampledomain', // domain name from canvas instance
-    clientId: '10000000000009', // clientid from the lti plugin which you get inside canvas after installing the plugin
-    authenticationEndpoint: 'https://canvas.exampledomain.com/api/lti/authorize_redirect',
-    accesstokenEndpoint: 'https://canvas.exampledomain.com/login/oauth2/token',
-    authConfig: { method: 'JWK_SET', key: 'https://canvas.exampledomain.com/api/lti/security/jwks' }
+    name: 'Canvas', // domain name from canvas instance
+    clientId: '10000000000015', // clientid from the lti plugin which you get inside canvas after installing the plugin
+    authenticationEndpoint:
+      'https://canvas-temp.pagewerkz.com/api/lti/authorize_redirect',
+    accesstokenEndpoint: 'https://canvas-temp.pagewerkz.com/login/oauth2/token',
+    authConfig: {
+      method: 'JWK_SET',
+      key: 'https://canvas-temp.pagewerkz.com/api/lti/security/jwks'
+    }
   })
-
-
 
   // For other plattforms, for example moodle:
 
